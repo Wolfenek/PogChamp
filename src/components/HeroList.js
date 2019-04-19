ecb27cc637567743a89612f41a513ffef8da4efd
Wolfenek <wolfenek@gmail.com>
2019-04-19 13:29:11 +0200

    added all heroes

--- src/components/HeroList.js
+++ src/components/HeroList.js
@@ -3,15 +3,13 @@ import HeroPortraits from "../images/HeroPortraits";
 
 const HeroList = () => {
   return (
-    <>
-      <ul>
-        {HeroPortraits.map(({ id, src, altTag }) => (
-          <li>
-            <img key={id} src={src} alt={altTag} />
-          </li>
-        ))}
-      </ul>
-    </>
+    <ul className="hero-list">
+      {HeroPortraits.map(({ id, src, altTag }) => (
+        <li>
+          <img key={id} src={src} alt={altTag} />
+        </li>
+      ))}
+    </ul>
   );
 };
 
--- src/images/HeroPortraits.js
+++ src/images/HeroPortraits.js
@@ -1,12 +1,65 @@
-import Ana from "./HeroIcons/icon-ana.png"
-import Ashe from "./HeroIcons/icon-ashe.png"
-import Baptiste from "./HeroIcons/icon-baptiste.png"
-
+import Ana from "./HeroIcons/icon-ana.png";
+import Ashe from "./HeroIcons/icon-ashe.png";
+import Baptiste from "./HeroIcons/icon-baptiste.png";
+import Bastion from "./HeroIcons/icon-bastion.png";
+import Brigitte from "./HeroIcons/icon-brigitte.png";
+import DVa from "./HeroIcons/icon-d.va.png";
+import Doomfist from "./HeroIcons/icon-doomfist.png";
+import Genji from "./HeroIcons/icon-genji.png";
+import Hanzo from "./HeroIcons/icon-hanzo.png";
+import Junkrat from "./HeroIcons/icon-junkrat.png";
+import Lucio from "./HeroIcons/icon-lucio.png";
+import McCree from "./HeroIcons/icon-mccree.png";
+import Mei from "./HeroIcons/icon-mei.png";
+import Mercy from "./HeroIcons/icon-mercy.png";
+import Moira from "./HeroIcons/icon-moira.png";
+import Orisa from "./HeroIcons/icon-orisa.png";
+import Pharah from "./HeroIcons/icon-pharah.png";
+import Reaper from "./HeroIcons/icon-reaper.png";
+import Reinhardt from "./HeroIcons/icon-reinhardt.png";
+import Roadhog from "./HeroIcons/icon-roadhog.png";
+import Soldier from "./HeroIcons/icon-soldier_76.png";
+import Sombra from "./HeroIcons/icon-sombra.png";
+import Symmetra from "./HeroIcons/icon-symmetra.png";
+import Torbjörn from "./HeroIcons/icon-torbjörn.png";
+import Tracer from "./HeroIcons/icon-tracer.png";
+import Widowmaker from "./HeroIcons/icon-widowmaker.png";
+import Winston from "./HeroIcons/icon-winston.png";
+import WreckingBall from "./HeroIcons/icon-wrecking_ball.png";
+import Zarya from "./HeroIcons/icon-zarya.png";
+import Zenyatta from "./HeroIcons/icon-zenyatta.png";
 
 const HeroPortraits = [
   { id: "ana", src: `${Ana}`, altTag: "ana overwatch" },
   { id: "ashe", src: `${Ashe}`, altTag: "ashe overwatch" },
-  { id: "baptiste", src: `${Baptiste}`, altTag: "baptiste overwatch" }
+  { id: "baptiste", src: `${Baptiste}`, altTag: "baptiste overwatch" },
+  { id: "bastion", src: `${Bastion}`, altTag: "bastion overwatch" },
+  { id: "brigitte", src: `${Brigitte}`, altTag: "brigitte overwatch" },
+  { id: "dva", src: `${DVa}`, altTag: "d.va overwatch" },
+  { id: "doomfist", src: `${Doomfist}`, altTag: "doomfist overwatch" },
+  { id: "genji", src: `${Genji}`, altTag: "genji genji" },
+  { id: "hanzo", src: `${Hanzo}`, altTag: "hanzo overwatch" },
+  { id: "junkrat", src: `${Junkrat}`, altTag: "junkrat overwatch" },
+  { id: "lucio", src: `${Lucio}`, altTag: "lucio overwatch" },
+  { id: "mccree", src: `${McCree}`, altTag: "mccree overwatch" },
+  { id: "mei", src: `${Mei}`, altTag: "mei overwatch" },
+  { id: "mercy", src: `${Mercy}`, altTag: "mercy overwatch" },
+  { id: "moira", src: `${Moira}`, altTag: "moira overwatch" },
+  { id: "orisa", src: `${Orisa}`, altTag: "orisa overwatch" },
+  { id: "pharah", src: `${Pharah}`, altTag: "pharah overwatch" },
+  { id: "reaper", src: `${Reaper}`, altTag: "reaper overwatch" },
+  { id: "reinhardt", src: `${Reinhardt}`, altTag: "reinhardt overwatch" },
+  { id: "roadhog", src: `${Roadhog}`, altTag: "roadhog overwatch" },
+  { id: "soldier", src: `${Soldier}`, altTag: "soldier 76 overwatch" },
+  { id: "sombra", src: `${Sombra}`, altTag: "sombra overwatch" },
+  { id: "symmetra", src: `${Symmetra}`, altTag: "symmetra overwatch" },
+  { id: "torbjörn", src: `${Torbjörn}`, altTag: "torbjörn overwatch" },
+  { id: "tracer", src: `${Tracer}`, altTag: "tracer overwatch" },
+  { id: "widowmaker", src: `${Widowmaker}`, altTag: "widowmaker overwatch" },
+  { id: "winston", src: `${Winston}`, altTag: "winston overwatch" },
+  { id: "wrecking ball", src: `${WreckingBall}`, altTag: "wrecking ball overwatch" },
+  { id: "zarya", src: `${Zarya}`, altTag: "zarya overwatch" },
+  { id: "zenyatta", src: `${Zenyatta}`, altTag: "zenyatta overwatch" }
 ];
 
 export default HeroPortraits;
