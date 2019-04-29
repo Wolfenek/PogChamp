6dcd0a457b27a7b7664d35f890e2a4ab599e4909
Wolfenek <wolfenek@gmail.com>
2019-04-29 14:47:00 +0200

    creating grids to display the content

--- src/components/HeroShowcase.js
+++ src/components/HeroShowcase.js
@@ -4,59 +4,48 @@ const HeroShowcase = props => {
   const { description } = props;
   console.log("from away", description);
 
-  // function getSkills(skills2) {
-  //   return Object.values(skills2)
-  //     .reduce((acc, skillset) => {
-  //       return [...acc, ...skillset];
-  //     }, [])
-  //     .map(skill => <li className="other-info">{skill}</li>);
-  // }
+  function showSkills() {
+    return description[0].skills.map(item => (
+      <>
+        <div className="skill-info-grid">
+          <div className="skill-icon">
+            <img src={item.img} />
+          </div>
+          <div className="skill-desc">
+            <h2>{item.name}</h2>
+          </div>
+        </div>
+        <li className="other-info">{item.name}</li>
+        <li className="other-info">{item.desc}</li>
+        <li className="other-info">Damage: {item.dmg}</li>
+        <li className="other-info">Healing: {item.healing}</li>
+        <li className="other-info">Headshot: {item.headshot}</li>
+      </>
+    ));
+  }
 
   const content = description.map(({ id, name, img, role, hp }) => (
     <>
-      <div className="self-center" key={id}>
+      <div className="main-info-grid" key={id}>
         <div className="hero-header">
           <h1>{name}</h1>
           <h2>Role: {role}</h2>
         </div>
-        <img src={img} />
+        <div>
+          <img src={img} />
+        </div>
       </div>
-      <ul>
+      <ul className="list-inline">
         <li>Total HP: {hp.total}</li>
         <li>Base HP: {hp.base_hp}</li>
         <li>Armor: {hp.armor}</li>
         <li>Shield: {hp.shield}</li>
       </ul>
-      <ul>
-        {description[0].skills.map(item => (
-          <>
-            <li className="other-info">
-              <img src={item.img} />
-            </li>
-            <li className="other-info">{item.name}</li>
-            <li className="other-info">{item.desc}</li>
-            <li className="other-info">Damage: {item.dmg}</li>
-            <li className="other-info">Healing: {item.healing}</li>
-            <li className="other-info">Headshot: {item.headshot}</li>
-          </>
-        ))}
-      </ul>
-      <hr />
-      {/* {description[0].skills[0].other.map(item => (
-          <>
-            <li className="other-info">{item}</li>
-          </>
-        ))} */}
-
-      <hr />
+      {showSkills()}
     </>
   ));
 
-  return (
-    <div>
-      <ul>{content}</ul>
-    </div>
-  );
+  return <div>{content}</div>;
 };
 
 export default HeroShowcase;
